40600f8e04ba2b58b7f1012d250c23835f4eed96
GODDONGHYUN <leenl1168@gmail.com>
2024-05-07 09:52:25 +0900

    2024-05-07

--- front/src/apis/board/index.ts
+++ front/src/apis/board/index.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 import { PostBoardRequestDto } from "./dto/request";
-import { POST_BOARD_REQUEST_URL } from "src/constant/Index";
+import {
+  GET_BOARD_LIST_URL,
+  GET_SEARCH_BOARD_LIST_URL,
+  POST_BOARD_REQUEST_URL,
+} from "src/constant/Index";
 import { bearerAuthorization, requestErrorHandler, requestHandler } from "..";
 import ResponseDto from "../response.dto";
-// function : Q&A 작성 API 함수
+import {
+  GetBoardListResponseDto,
+  GetSearchBoardResponseDto,
+} from "./dto/response";
+
+// function: Q&A 작성 API 함수
 export const postBoardRequest = async (
   requestBody: PostBoardRequestDto,
   accessToken: string
@@ -11,6 +20,30 @@ export const postBoardRequest = async (
   const result = await axios
     .post(POST_BOARD_REQUEST_URL, requestBody, bearerAuthorization(accessToken))
     .then(requestHandler<ResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function: Q&A 전체 리스트 불러오기 API 함수
+export const getBoardListRequest = async (accessToken: string) => {
+  const result = await axios
+    .get(GET_BOARD_LIST_URL, bearerAuthorization(accessToken))
+    .then(requestHandler<GetBoardListResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function: Q&A 검색 리스트 불러오기 API 함수
+export const getSearchBoardListRequest = async (
+  searchWord: string,
+  accessToken: string
+) => {
+  const result = await axios
+    .get(
+      GET_SEARCH_BOARD_LIST_URL(searchWord),
+      bearerAuthorization(accessToken)
+    )
+    .then(requestHandler<GetSearchBoardResponseDto>)
     .catch(requestErrorHandler);
   return result;
 };
